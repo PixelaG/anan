918f4d224f09bfde4e9fe2052343b6fbe7791f37
PixelaG <143560379+PixelaG@users.noreply.github.com>
2026-02-13 23:58:04 +0400

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from "react";
-
+import lovesvg from "./assets/All You Need Is Love SVG Cut File.png";
+import lovesvg2 from "./assets/Love In The Air SVG Cut File.png";
 export default function Page() {
   const [noCount, setNoCount] = useState(0);
   const [yesPressed, setYesPressed] = useState(false);
   const yesButtonSize = noCount * 20 + 16;
-
   const handleNoClick = () => {
     setNoCount(noCount + 1);
   };
-
   const getNoButtonText = () => {
     const phrases = [
       "არა",
@@ -28,62 +27,67 @@ export default function Page() {
       "გულს მიტეხ ;(",
       "გთხოვ? :( გულს მიკლავ",
     ];
-
     return phrases[Math.min(noCount, phrases.length - 1)];
   };
-
   return (
     <div className="overflow-hidden flex flex-col items-center justify-center pt-4 h-screen -mt-16 selection:bg-rose-600 selection:text-white text-zinc-900">
+      {" "}
       {yesPressed ? (
         <>
+          {" "}
           <img
-            src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.webp"
+            src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif"
             className="mb-6"
-          />
+          />{" "}
           <div className="text-4xl md:text-6xl font-bold text-center flex flex-col items-center space-y-4">
-            <span>იესსსსსსს!!!</span>
-            <span>ვიცოდი რომ დამთანხმდებოდი 🫶😘💘</span>
-          </div>
+            {" "}
+            <span>იესსსსსსს!!!</span>{" "}
+            <span>ვიცოდი რომ დამთანხმდებოდი 🫶😘💘</span>{" "}
+          </div>{" "}
         </>
       ) : (
         <>
+          {" "}
           <img
-            src="/lovesvg.png"
+            src={lovesvg}
             className="fixed animate-pulse top-10 md:left-24 left-6 md:w-40 w-28"
-          />
+          />{" "}
           <img
-            src="/lovesvg2.png"
+            src={lovesvg2}
             className="fixed bottom-16 -z-10 animate-pulse md:right-24 right-10 md:w-40 w-32"
-            />
+          />{" "}
           <img
-            src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.webp"
             className="h-[230px] rounded-lg shadow-lg"
-          />
+            src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.webp"
+          />{" "}
           <h1 className="text-4xl md:text-6xl my-4 text-center">
-            ანა, იქნები ჩემი ვალენტინი? 😍🥰
-          </h1>
+            {" "}
+            ანა, იქნები ჩემი ვალენტინი? 😍🥰{" "}
+          </h1>{" "}
           <div className="flex flex-wrap justify-center gap-2 items-center">
+            {" "}
             <button
               className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-lg mr-4"
               style={{ fontSize: yesButtonSize }}
               onClick={() => setYesPressed(true)}
             >
-              კი 😘
-            </button>
+              {" "}
+              კი 😘{" "}
+            </button>{" "}
             <button
               onClick={handleNoClick}
               className="bg-rose-500 hover:bg-rose-600 rounded-lg text-white font-bold py-2 px-4"
             >
-              {noCount === 0 ? "არა" : getNoButtonText()}
-            </button>
-          </div>
+              {" "}
+              {noCount === 0 ? "არა" : getNoButtonText()}{" "}
+            </button>{" "}
+          </div>{" "}
         </>
-      )}
-      <Footer />
+      )}{" "}
+      <Footer />{" "}
     </div>
   );
 }
-
 const Footer = () => {
   return (
     <a
@@ -91,10 +95,12 @@ const Footer = () => {
       href="https://github.com/Xeven777/valentine"
       target="__blank"
     >
+      {" "}
       დაამზადა FIXUXI - იმ{" "}
       <span role="img" aria-label="heart">
-        ❤️
-      </span>
+        {" "}
+        ❤️{" "}
+      </span>{" "}
     </a>
   );
 };
